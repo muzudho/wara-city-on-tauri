3b928a2892f9e3f8bc84e7b66261e31450ae4dd0
muzudho <muzudho1@gmail.com>
2025-07-25 16:12:50 +0900

    tiles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
     <v-app>
         <v-main>
-    <!--
-            <CsvTable />
-    -->
             <v-container class="pa-1">
                 <v-row no-gutters>
                     <v-col><v-text-field v-model="filePathVM" label="File Path" class="pa-0"></v-text-field></v-col>
@@ -11,6 +8,117 @@
                     <v-col cols="2"><v-btn block v-on:click="onRefreshButtonClicked" class="pa-0">Refresh</v-btn></v-col>
                     <v-col cols="2"><v-btn block v-on:click="onWriteButtonClicked" class="pa-0">Write</v-btn></v-col>
                 </v-row>
+                <v-container class="pa-0" style="position:relative; width:320px; height:320px; overflow: hidden; background-color:black;">
+                    <v-img src="/public/img/tiles/wasteland.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:0px; left:0px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:0px; left:32px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:0px; left:64px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:0px; left:96px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:0px; left:128px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:0px; left:160px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:0px; left:192px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:0px; left:224px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:0px; left:256px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:0px; left:288px;" no-gutters></v-img>
+
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:32px; left:0px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/wasteland.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:32px; left:32px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:32px; left:64px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:32px; left:96px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:32px; left:128px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:32px; left:160px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:32px; left:192px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:32px; left:224px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:32px; left:256px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:32px; left:288px;" no-gutters></v-img>
+
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:64px; left:0px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:64px; left:32px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/wasteland.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:64px; left:64px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:64px; left:96px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:64px; left:128px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:64px; left:160px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:64px; left:192px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:64px; left:224px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:64px; left:256px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:64px; left:288px;" no-gutters></v-img>
+
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:96px; left:0px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:96px; left:32px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:96px; left:64px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/wasteland.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:96px; left:96px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:96px; left:128px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:96px; left:160px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:96px; left:192px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:96px; left:224px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:96px; left:256px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:96px; left:288px;" no-gutters></v-img>
+
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:128px; left:0px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:128px; left:32px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:128px; left:64px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:128px; left:96px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/wasteland.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:128px; left:128px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:128px; left:160px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:128px; left:192px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:128px; left:224px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:128px; left:256px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:128px; left:288px;" no-gutters></v-img>
+
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:160px; left:0px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:160px; left:32px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:160px; left:64px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:160px; left:96px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:160px; left:128px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/wasteland.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:160px; left:160px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:160px; left:192px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:160px; left:224px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:160px; left:256px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:160px; left:288px;" no-gutters></v-img>
+
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:192px; left:0px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:192px; left:32px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:192px; left:64px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:192px; left:96px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:192px; left:128px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:192px; left:160px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/wasteland.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:192px; left:192px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:192px; left:224px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:192px; left:256px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:192px; left:288px;" no-gutters></v-img>
+
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:224px; left:0px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:224px; left:32px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:224px; left:64px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:224px; left:96px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:224px; left:128px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:224px; left:160px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:224px; left:192px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/wasteland.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:224px; left:224px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:224px; left:256px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:224px; left:288px;" no-gutters></v-img>
+
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:256px; left:0px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:256px; left:32px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:256px; left:64px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:256px; left:96px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:256px; left:128px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:256px; left:160px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:256px; left:192px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:256px; left:224px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/wasteland.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:256px; left:256px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:256px; left:288px;" no-gutters></v-img>
+
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:288px; left:0px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:288px; left:32px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:288px; left:64px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:288px; left:96px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:288px; left:128px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:288px; left:160px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:288px; left:192px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:288px; left:224px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/sea.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:288px; left:256px;" no-gutters></v-img>
+                    <v-img src="/public/img/tiles/wasteland.png" width="32" height="32" class="ma-0" aspect-ratio="1" style="position:absolute; top:288px; left:288px;" no-gutters></v-img>
+                </v-container>
                 <v-row no-gutters>
                     <v-textarea v-model="textVM"></v-textarea>
                 </v-row>
