3c02ee7d7f40528cf12151302356fa829ba73a32
muzudho <muzudho1@gmail.com>
2025-07-26 22:48:34 +0900

    WIP: Tile 練習中

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -13,15 +13,18 @@
     -->
 </template>
 
-<script setup>
+<script setup lang="ts">
     import { computed } from 'vue';
 
-    defineProps({
+    /*
+    interface TileArgs {
         tileX: { type: Number, required: true }, // タイルのX座標（グリッド）
         tileY: { type: Number, required: true }, // タイルのY座標（グリッド）
         tileSize: { type: Number, default: 32 }, // タイルの幅と高さ（px）
         tilemapUrl: { type: String, required: true }, // タイルマップ画像のURL
-    });
+    }
+    const props = defineProps<TileArgs>();
+    // */
 
     /*
     const tileStyle = computed(() => ({
@@ -29,6 +32,16 @@
         height: `${props.tileSize}px`,
         backgroundImage: `url('${props.tilemapUrl}')`,
         backgroundPosition: `${-props.tileX * props.tileSize}px ${-props.tileY * props.tileSize}px`,
+        backgroundRepeat: 'no-repeat',
+    }));
+    // */
+
+    /*
+    const tileStyle = computed(() => ({
+        width: `${props.tileSize}px`,
+        height: `${props.tileSize}px`,
+        backgroundImage: `url('/public/img/tiles/tilemap1.png')`,
+        backgroundPosition: `-32px 0px`,
         backgroundRepeat: 'no-repeat',
     }));
     // */
